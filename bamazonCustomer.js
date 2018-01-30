69ca9592aec49e76f79537b303cf38226c583953
Brandon Haines <haines.brandon.91@gmail.com>
2018-01-30 12:19:32 -0700

    created required vars

---
+++ bamazonCustomer.js
@@ -0,0 +1,16 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var colors = require("colors");
+
+colors.setTheme({
+	silly: 'rainbow',
+	input: 'grey',
+	verbose: 'cyan',
+	prompt: 'grey',
+	info: 'green',
+	data: 'grey',
+	help: 'cyan',
+	warn: 'yellow',
+	debug: 'blue',
+	error: 'red'
+});
