24e58e6cbdbe38ac3715f8f3767eade8467bf710
Brandon Haines <haines.brandon.91@gmail.com>
2018-01-30 12:35:16 -0700

    updated schema

--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -11,7 +11,7 @@ CREATE TABLE products(
     PRIMARY KEY (item_id)
 );
 
-INSERT INTO products(product_name, department_name, price, stock_quantity, product_sales)
+INSERT INTO products(product_name, department_name, price, stock_quantity)
 VALUES ("4k TV", "Electronics", 1599.99, 20), ("Cleats", "Apparel", 99.99, 52), ("Xbox", "Electronics", 400.00, 30), ("Dog Food", "Pets", 25.00, 226), ("iPhone 29", "Cell Phones", 4359.35, 15), ("Kleenex", "Toiletries", 5.99, 2500);
 
 
